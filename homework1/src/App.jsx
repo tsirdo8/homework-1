7b14c0d308b98e40b0b8066ffc9a7cf1a91c10cd
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2024-09-27 15:04:07 +0400

    homework 28.09.24

---
+++ homework1/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">ნიკოლოზ ცირდავა, 15 წლის</h1>
+      <p className="paragraph">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+      </p>
+
+      <h1 className="title">მიყვარს კოდის წერა</h1>
+      <p className="paragraph">მომწონს პითონი და ჯავასკრიფტი</p>
+
+      <h1 className="title">ფეხბურთი</h1>
+      <p className="paragraph">ვარ ბარსელონას ფანი</p>
+
+      <p className="paragraph">საყვარელი მოთამაშე: ლეო მესი</p>
+    </div>
+  );
+}
+
+export default App;
